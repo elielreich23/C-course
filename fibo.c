34c0e8cac63bba56091d3bc847ea299e5326958e
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2023-08-14 15:12:21 -0300

    Add files via upload

---
+++ fibo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+    int n, x, a, b;
+
+    printf("DIgite o termo desejado");
+    scanf("%d", &a);
+}
+
+int fibo(int y){
+    if(a>0 && a <3){
+        return 1;
+    }
+    else if {
+        n = 1;
+        x = n;
+
+    }
+}
