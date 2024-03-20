a2810df537face6427daf966a1fb38c4324c9eac
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2024-03-20 16:38:41 -0300

    EDA001

---
+++ struct.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *prox;
+};
+
+int main()
+{
+    struct Node *node = (struct Node*)malloc(sizeof(struct Node));
+    
+    if (node == NULL){
+        printf("error ao alloca memoria\n");
+        return 1;
+    }
+    
+    node ->data = 1; 
+node -> prox = NULL;
+printf("valor de data : %d\n", node ->data);
+printf("valor do proximo ponteiro: %p\n", node->prox);
+
+free(node);
+
+return 0:
+}
